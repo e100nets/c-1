c354bc436b73d82f11a108934723abd7a1d85575
Игнатов Илья <64230361+e100nets@users.noreply.github.com>
2020-10-13 02:11:24 +0300

    Add files via upload

---
+++ task1.1/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace logic
+{
+    public class Logic 
+    {
+        public double Distance(double boatSpeed, double flowSpeed, double timeOnRiver, double timeOnLake)
+        {
+            double distance;
+            if (boatSpeed > 0 && timeOnLake > 0 && timeOnRiver > 0 && boatSpeed - flowSpeed > 0)
+            
+                distance = (boatSpeed - flowSpeed) * timeOnRiver + boatSpeed * timeOnLake;
+            
+            else
+                distance = 0;
+
+            return distance;
+        }
+    }
+}
